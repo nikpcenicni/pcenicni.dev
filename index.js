33a930ef9bea2734f1652c433d3b087e5988a6a6
Nikholas Pcenicni <nik.pcenicni@icloud.com>
2021-05-24 04:54:33 +0000

    neumorphism

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function shownavmenu() {
         x.className = "topnav";
     }
 }
+
+var btn = document.querySelector("#darkmode");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", function () {
+if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+} else {
+    document.body.classList.toggle("dark-theme");
+}
+});
