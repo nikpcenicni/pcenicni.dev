c641b16061d35b728542ba1baaa7c23780ad0686
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2023-03-23 21:01:52 -0400

    Rebased

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+        'main': '#f8f5f2',
+        'stroke': '#232323',
+        'text': '#232323',
+        'greenblue': '#078080',
+        'redorange': '#f45d48'
+
+      // Add more color palettes as needed
+    },
   },
   plugins: [],
 }
