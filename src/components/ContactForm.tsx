29f142d7091bb1ee0f29bd3854ce34fccf8148da
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2023-03-24 00:08:13 -0400

    maybe fix

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -19,13 +19,14 @@ const ContactForm = ({ fields, buttonText }: ContactFormProps) => {
       <div className='mt-10 w-1/2 mx-auto border-4 rounded-xl'>
         <h1 className='text-2xl font-bold mb-2 p-4 border-b-4 bg-greenblue text-main border-stroke rounded-t-lg'>Tell Me About It</h1>
         
-        <form data-netlify="true" name="contact" method="POST"className="p-4">
+        <form name="contact" method="POST" data-netlify="true" className="p-4">
           {fields.map(({ name, label, type, required }) => (
             <div key={name} className="mb-4">
               <label htmlFor={name} className="block mb-2 font-semibold">{label}</label>
               <input
                 type={type}
                 id={name}
+                name={name} // add this line
                 required={required}
                 className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
