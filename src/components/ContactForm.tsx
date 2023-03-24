2f305741d4f7967fa602065687540097c75a1df0
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2023-03-24 01:13:51 -0400

    test parameterized contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -10,34 +10,60 @@ interface FieldProps {
 }
 
 const ContactForm: React.FC<{ fields: FieldProps[] }> = ({ fields }) => {
+
+  const [formData, setFormData] = useState({});
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.post("/", formData).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="pt-20">
       <div className='mt-10 w-1/2 mx-auto border-4 rounded-xl'>
         <h1 className='text-2xl font-bold mb-2 p-4 border-b-4 bg-greenblue text-main border-stroke rounded-t-lg'>Tell Me About It</h1>
         <form name="contact" action="/success" method="POST" data-netlify="true" className="p-4">
             <input type="hidden" name="form-name" value="contact" />
+            {fields.map((field) => (
+            <div key={field.name} className="mt-4">
+              <label htmlFor={field.name} className="block mb-2 font-semibold">
+                {field.label}:
+              </label>
+              {field.type === "textarea" ? (
+                <textarea
+                  name={field.name}
+                  id={field.name}
+                  required={field.required}
+                  placeholder={field.placeholder}
+                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <input
+                  type={field.type}
+                  name={field.name}
+                  id={field.name}
+                  required={field.required}
+                  placeholder={field.placeholder}
+                  className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  onChange={handleInputChange}
+                />
+              )}
+            </div>
+          ))}
 
-            <div>
-              <label htmlFor="firstname" className="block mb-2 font-semibold">
-                Your Name:
-              </label>
-              <input type="text" name="name" id="firstname" required className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-            </div>
-            <div>
-              <label htmlFor="youremail" className="block mb-2 font-semibold">
-                Your Email:
-              </label>
-              <input type="email" name="email" id="youremail" required className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-            </div>
-              <div>
-              <label htmlFor="yourmessage" className="block mb-2 font-semibold">
-                Message:
-              </label>
-              <textarea name="message" required id="yourmessage" className="w-full px-3 py-2 border rounded-md foucs:outline-none focus:ring-2 foucus:ring-blue-500"></textarea>
-            </div>
-            <div className="mt-4 w-full text-center">
-              <button type="submit" className="font-bold bg-greenblue text-main rounded-lg py-2 px-8">Send</button>
-            </div>
+          <div className="mt-4 w-full text-center">
+            <button type="submit" className="font-bold bg-greenblue text-main rounded-lg py-2 px-8">
+              Send
+            </button>
+          </div>
             
         </form>
       </div>
