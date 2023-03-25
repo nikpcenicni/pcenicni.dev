9b8bde646842ecb70f76a516c2d0e1edb5451721
Nikholas Pcenicni <82239765+nikpcenicni@users.noreply.github.com>
2023-03-24 20:43:37 -0400

    again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-greenblue text-white mt-8 py-4 px-6 flex flex-col sm:flex-row items-center w-full bottom-0">
+    <footer className="bg-greenblue text-white mt-8 py-4 px-6 flex flex-col sm:flex-row items-center w-full bottom-0 mt-10">
       <p className="mb-4 sm:mb-0 text-center">&copy; {currentYear} Pcenicni.dev</p>
       <nav className="ml-auto">
         <div className='mx-2'>            
